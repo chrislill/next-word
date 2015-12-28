80204869b0d229801e5a8646754b428e0300417e
chrislill <chris_lill_uk@hotmail.com>
2015-12-27 22:48:08 -0500

    Delete punctuation-tests.R

--- punctuation-tests.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-
-expect_equal(punctuationRegex("Just like"), "Just like")
-expect_equal(punctuationRegex("coast, the"), "coast the")
-expect_equal(punctuationRegex("don't"), "don't")
-expect_equal(punctuationRegex("you???re"), "you're")
-expect_equal(punctuationRegex("thick-walled"), "thick-walled")
-expect_equal(punctuationRegex("thick -walled"), "thick walled")
-expect_equal(punctuationRegex("thick- walled"), "thick walled")
