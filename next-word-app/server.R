cbd6fcc3d2c300bf25a5f3cbbd80c89de3c1c2ed
chrislill <chris_lill_uk@hotmail.com>
2016-01-14 21:24:55 +0000

    Improvements to shiny app

--- next-word-app/server.R
+++ next-word-app/server.R
@@ -8,15 +8,40 @@ load("..\\data\\prediction.RData")
 
 shinyServer(function(input, output, session) {
   
-  observe({
-    updateTextInput(session, "sentence", value = paste(input$sentence, "sea"))  
+  choice <- reactiveValues(word = NULL)
+  answers <- unlist(prediction[1, 3:7, with = FALSE])
+  probabilities <- unlist(round(prediction[1, 8:12, with = FALSE] * 100))
+  
+  observeEvent(input$answer1, { choice$word <- answers[1] })
+  observeEvent(input$answer2, { choice$word <- answers[2] })
+  observeEvent(input$answer3, { choice$word <- answers[3] })
+  observeEvent(input$answer4, { choice$word <- answers[4] })
+  observeEvent(input$answer5, { choice$word <- answers[5] })
+  
+    #     updateTextInput(session, "sentence", value = paste(input$sentence, "sea"))  
     
+  output$tokens <- renderText({
+    paste(unlist(TokeniseText(input$sentence)))
   })
   
-  # output$renderText({})
+  output$answers <- renderText({
+    paste(prediction[,3:7, with = FALSE])
+  })
   
-})
+  output$clicked <- renderText({
+    choice$word
+  })
+  
+  output$button1 <- renderUI({ actionButton("answer1", label = answers[1]) })
+  output$button2 <- renderUI({ actionButton("answer2", label = answers[2]) })
+  output$button3 <- renderUI({ actionButton("answer3", label = answers[3]) })
+  output$button4 <- renderUI({ actionButton("answer4", label = answers[4]) })
+  output$button5 <- renderUI({ actionButton("answer5", label = answers[5]) })
+  
+  output$prob1 <- renderText({ paste0(probabilities[1], "%") })
+  output$prob2 <- renderText({ paste0(probabilities[2], "%") })
+  output$prob3 <- renderText({ paste0(probabilities[3], "%") })
+  output$prob4 <- renderText({ paste0(probabilities[4], "%") })
+  output$prob5 <- renderText({ paste0(probabilities[5], "%") })
 
-# http://stackoverflow.com/questions/27326929/how-to-update-button-labels-in-r-shiny
-# http://stackoverflow.com/questions/33777991/update-label-of-actionbutton-in-shiny
-# http://stackoverflow.com/questions/29376031/capture-the-label-of-an-actionbutton-once-it-is-clicked
+})
