202bf9f70cbb0ec02ee083b6532da14d21bccae1
chrislill <chris_lill_uk@hotmail.com>
2016-01-17 21:45:40 +0000

    Improved the textbox focus javascript

--- next-word-app/www/helper.js
+++ next-word-app/www/helper.js
@@ -1,3 +1,7 @@
 function focusOnTextBox() {
-    document.getElementById("sentence").focus();
- }
+    textbox = document.getElementById("sentence")
+    textbox.focus();
+    var val = textbox.value; //store the value of the element
+    textbox.value = ''; //clear the value of the element
+    textbox.value = val; //set that value back. 
+}
